2b5c1b7cc093db8a2fa96e6a87733da96585d4d1
RABINARAYAN SAHOO <rabinarayansahoo9348@gmail.com>
2025-06-02 08:39:17 +0530

    Update firebase.js

--- unit-4/pai-01/src/firebase.js
+++ unit-4/pai-01/src/firebase.js
@@ -3,15 +3,15 @@ import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAip-2cEuNHV3lq670l4RG8L1jZ2qyAoEM",
-  authDomain: "userdb-67b43.firebaseapp.com",
+  apiKey: "",
+  authDomain: "",
   databaseURL:
-    "https://userdb-67b43-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "userdb-67b43",
-  storageBucket: "userdb-67b43.firebasestorage.app",
-  messagingSenderId: "716340306235",
-  appId: "1:716340306235:web:f924df28dd04e9d3acd71e",
-  measurementId: "G-DQ7ND95RJF",
+    "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
